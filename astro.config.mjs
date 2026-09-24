b4b38bdc09656517fbb421d86e61d1540a01122f
cyberbaker <cyberbaker@pm.me>
2026-09-24 14:15:39 +0100

    Change of domain

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import { defineConfig, fontProviders } from 'astro/config';
 export default defineConfig({
 	// Your public URL. For a repo named <username>.github.io this is https://<username>.github.io
 	// If you use a custom domain, put it here instead. Must match `siteUrl` in the n8n Config node.
-	site: 'https://cyberbaker.github.io',
+	site: 'https://cyberbaker.co.uk',
 	integrations: [mdx(), sitemap()],
 	fonts: [
 		{
